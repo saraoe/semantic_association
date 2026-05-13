a30f8584f679407bb91de491e5ed838e10f65471
saraoe <sara.m.oestergaard@gmail.com>
2026-05-13 12:19:36 +0200

    init regression models exp 2

---
+++ experiment2/analysis/regression_models.r
@@ -0,0 +1,134 @@
+### Bayesian hierarchical regression models ###
+
+library(tidytable)
+library(brms)
+library(stringr)
+library(argparse)
+
+setwd("experiment2")
+
+options(mc.cores = parallel::detectCores())
+options(brms.backend = "cmdstan")
+
+## Specify dependent variables using argparse
+parser <- ArgumentParser(description = "Run brms models")
+parser$add_argument("--dataset",
+    type = "character",
+    nargs = "+",
+    default = c("derco", "tanner", "p600"),
+    help = "Specify dataset (derco or tanner)"
+)
+
+args <- parser$parse_args()
+dataset <- args$dataset
+
+print(paste(
+    "Running models for dataset: ",
+    dataset,
+    sep = ""
+))
+
+# create out folder
+out_folder <- file.path("analysis", "brms_models")
+if (!dir.exists(out_folder)) {
+    dir.create(out_folder)
+}
+
+# prior
+erp_priors <- c(
+    prior(normal(0, 20), class = Intercept),
+    prior(normal(0, 10), class = b),
+    prior(normal(0, 10), class = sigma),
+    prior(normal(0, 10), class = sd)
+)
+
+if ("tanner" %in% dataset) {
+    print("Dataset: Tanner")
+    # load data
+    tanner_sem <- read.csv(
+        file.path("results", "tanner_semantic_association.csv")
+    ) |>
+        select(-X) |>
+        mutate(
+            implementation_id = paste(implementation, model, sep = "_")
+        ) |>
+        mutate(implementation_id = str_replace(implementation_id, "/", "_"))
+    tanner_df <- read.csv(file.path("data", "Tanner", "mean_amplitude.csv")) |>
+        mutate(context = ifelse(is.na(context), "", context)) |>
+        left_join(tanner_sem) |>
+        filter(Acceptability == "Gram")
+
+    # model formula
+    sem_formula <- bf(
+        n400 ~ s_sem +
+            (s_sem || subject) +
+            (s_sem || id) +
+            (s_sem || word)
+    )
+
+    # run models
+    implementations <- tanner_df |>
+        pull(implementation_id) |>
+        unique()
+    for (imp_id in implementations) {
+        print(paste("Running implementation", imp_id))
+        data <- tanner_df |>
+            filter(implementation_id == imp_id) |>
+            mutate(s_sem = scale(semantic_association))
+
+        m <- brm(sem_formula,
+            family = gaussian(),
+            prior = erp_priors,
+            data = data,
+            chains = 4,
+            control = list(adapt_delta = 0.9999),
+            seed = 246,
+            file = file.path(out_folder, paste0("tanner_", imp_id))
+        )
+    }
+}
+
+
+if ("derco" %in% dataset) {
+    # load data
+    derco_sem <- read.csv(
+        file.path("results", "derco_semantic_association.csv")
+    ) |>
+        select(-X) |>
+        mutate(
+            implementation_id = paste(implementation, model, sep = "_")
+        ) |>
+        mutate(implementation_id = str_replace(implementation_id, "/", "_"))
+    derco_df <- read.csv(
+        file.path("data", "DERCo", "mean_amplitude.csv")
+    ) |>
+        left_join(derco_sem)
+
+    # model formula
+    sem_formula <- bf(
+        n400 ~ s_sem +
+            (s_sem || subject) +
+            (s_sem || article_n) +
+            (s_sem || word)
+    )
+
+    # run models
+    implementations <- derco_df |>
+        pull(implementation_id) |>
+        unique()
+    for (imp_id in implementations) {
+        data <- derco_df |>
+            filter(implementation_id == imp_id) |>
+            mutate(s_sem = scale(semantic_association))
+
+        m <- brm(sem_formula,
+            family = gaussian(),
+            prior = erp_priors,
+            data = data,
+            chains = 4,
+            control = list(adapt_delta = 0.9999),
+            seed = 246,
+            file = file.path(out_folder, paste0("derco_", imp_id))
+        )
+    }
+}
