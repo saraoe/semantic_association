5c839b2ad9508f511b0db1b6024a7a61b89e0f18
saraoe <sara.m.oestergaard@gmail.com>
2026-07-17 13:36:30 +0200

    ucl models + clean word

--- experiment2/analysis/regression_models.r
+++ experiment2/analysis/regression_models.r
@@ -15,7 +15,7 @@ parser <- ArgumentParser(description = "Run brms models")
 parser$add_argument("--dataset",
     type = "character",
     nargs = "+",
-    default = c("derco", "tanner", "p600"),
+    default = c("derco", "tanner", "ucl"),
     help = "Specify dataset (derco or tanner)"
 )
 
@@ -32,6 +32,13 @@ print(paste(
 out_folder <- file.path("analysis", "brms_models")
 if (!dir.exists(out_folder)) {
     dir.create(out_folder)
+}
+
+# function for cleaning word
+clean_word <- function(word) {
+    word |>
+        tolower() |>
+        gsub("[[:punct:]]", "", x = _)
 }
 
 # prior
@@ -60,7 +67,8 @@ if ("tanner" %in% dataset) {
         mutate(context = ifelse(is.na(context), "", context)) |>
         left_join(tanner_sem) |>
         filter(Acceptability == "Gram") |>
-        filter(pos %in% content_pos)
+        filter(pos %in% content_pos) |>
+        mutate(word = clean_word(word))
 
     # model formula
     sem_formula <- bf(
@@ -122,6 +130,72 @@ if ("tanner" %in% dataset) {
     }
 }
 
+if ("ucl" %in% dataset) {
+    print("Dataset: UCL")
+    # load data
+    ucl_sem <- read.csv(
+        file.path("results", "ucl_semantic_association.csv")
+    ) |>
+        select(-X) |>
+        mutate(
+            implementation_id = paste(implementation, model, sep = "_")
+        ) |>
+        mutate(implementation_id = str_replace(implementation_id, "/", "_"))
+    ucl_df <- read.csv(file.path("data", "UCL", "mean_amplitude.csv")) |>
+        left_join(ucl_sem) |>
+        filter(pos %in% content_pos) |>
+        mutate(word = clean_word(word)) |>
+        rename("n400" = "N400")
+
+    # model formula
+    sem_formula <- bf(
+        n400 ~ s_sem +
+            (s_sem || subject) +
+            (s_sem || id) +
+            (s_sem || word)
+    )
+
+    sem_lp_formula <- bf(
+        n400 ~ s_sem + s_lp +
+            (s_sem + s_lp || subject) +
+            (s_sem + s_lp || id) +
+            (s_sem + s_lp || word)
+    )
+
+    # run models
+    implementations <- ucl_df |>
+        pull(implementation_id) |>
+        unique()
+    for (imp_id in implementations) {
+        print(paste("Running implementation", imp_id))
+        data <- ucl_df |>
+            filter(implementation_id == imp_id) |>
+            mutate(s_sem = scale(semantic_association))
+
+        # n400 ~ sem
+        m_sem <- brm(sem_formula,
+            family = gaussian(),
+            prior = erp_priors,
+            data = data,
+            chains = 4,
+            control = list(adapt_delta = 0.9999),
+            seed = 246,
+            file = file.path(out_folder, paste0("ucl_", imp_id))
+        )
+
+        # n400 ~ sem + lp
+        m_sem_lp <- brm(sem_lp_formula,
+            family = gaussian(),
+            prior = erp_priors,
+            data = data,
+            chains = 4,
+            control = list(adapt_delta = 0.9999),
+            seed = 246,
+            file = file.path(out_folder, paste0("ucl_lp_", imp_id))
+        )
+    }
+}
+
 
 if ("derco" %in% dataset) {
     # load data
@@ -137,7 +211,8 @@ if ("derco" %in% dataset) {
         file.path("data", "DERCo", "mean_amplitude.csv")
     ) |>
         left_join(derco_sem) |>
-        filter(pos %in% content_pos)
+        filter(pos %in% content_pos) |>
+        mutate(word = clean_word(target))
 
     # model formula
     sem_formula <- bf(
